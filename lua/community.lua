906bb763459c1b72c98c30931ccb15e4d9b06fc0
line <zhaochenxian@outlook.com>
2025-10-29 20:08:54 +0800

    我添加了一点python的配置

--- lua/community.lua
+++ lua/community.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
@@ -9,4 +7,10 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
+  { import = "astrocommunity.recipes.vscode" },
+  { import = "astrocommunity.completion.copilot-vim-cmp" },
+  { import = "astrocommunity.completion.blink-cmp" },
+  { import = "astrocommunity.completion.blink-cmp-tmux" },
+  { import = "astrocommunity.completion.blink-cmp-git" },
+  { import = "astrocommunity.completion.blink-cmp-emoji" },
 }
